4cac65d159cb07cf9f44750bf21e217f6cde03eb
sagar999911 <80348871+sagar999911@users.noreply.github.com>
2021-07-08 19:13:36 +0530

    Update pdf_extraction.py

--- pdf_extraction.py
+++ pdf_extraction.py
@@ -5,28 +5,27 @@ pdf_name = 'montly_statement.pdf'
 with pdfplumber.open(pdf_name) as pdf:
     """Getting texts like Customer Name and Address"""
     page = pdf.pages[0]
-    text = page.extract_text()
+    cust_details = page.extract_text()
     # print(text)
-    newtext = text.split('\n')
+    cust_details = cust_details.split('\n')
     # print(newtext)
-    pin = newtext[15].split()
-    pin = pin[0]
-    print(f"Customer Name: {newtext[4]}\nAddress: {newtext[6]}, {newtext[10]}, {newtext[12]}, {pin} ")
+    pin_code = cust_details[15].split()
+    pin_code = pin_code[0]
+    print(f"Customer Name: {cust_details[4]}\nAddress: {cust_details[6]}, {cust_details[10]}, {cust_details[12]}, {pin_code}")
 
     """Extracting first two Transactions with all details from table"""
     print('----First two Transactions-----')
 
-    lst = []
+    transaction_lst = []
     for i in page.extract_table():
-        lst.append(i)
+        transaction_lst.append(i)
 
-    for j in lst[1:3]:
-        dict = {'Date': j[0],
-                'Narration': j[1],
-                'Chq/Ref No.': j[2],
-                'Value Date': j[3],
-                'Withdrawl Amt': j[4],
-                'Deposit Amt': j[5],
-                'Closing Balance': j[6]
-                }
-        print(dict)
+    for j in transaction_lst[1:3]:
+        transactions = {'Date': j[0],
+                        'Narration': j[1],
+                        'Chq/Ref No.': j[2],
+                        'Value Date': j[3],
+                        'Withdrawl Amt': j[4],
+                        'Deposit Amt': j[5],
+                        'Closing Balance': j[6]}
+        print(transactions)
